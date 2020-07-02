7e7a073dbdb508cb92ba626f69fc0b98a3181d8d
Stevie <xxlennygamerxx@gmail.com>
2020-07-02 16:57:00 -0700

    Abstract classes

---
+++ Items/GunParts/Attachments/Sights/SightAttachment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gunplay.Items.GunParts.Attachments.Sights
+{
+    // Universal sight attachment code goes here.
+    public abstract class SightAttachment : AttachmentPart
+    {
+    }
+}
